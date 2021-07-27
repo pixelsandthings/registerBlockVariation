e6b68fc3e1f0220acbe010704a5b1990647aa86f
Paul Jackson <paul@pixelsandthings.co.uk>
2021-07-27 21:59:35 +0100

    Create registerBlockVariation.js

---
+++ registerBlockVariation.js
@@ -0,0 +1,31 @@
+wp.blocks.registerBlockVariation(
+		'core/columns', {
+			name: 'blockname',
+			title: 'Block Name',
+			icon: 'dashicons-icon-class',
+			attributes: {
+				cover: 'https://example.com/image.jpg',
+				align: 'wide',
+				className: 'blockname'
+			},
+			scope: ['inserter'],
+			innerBlocks: [
+				['core/column', { "width": 60, className: 'internal-block-class' },
+					[
+						['core/heading', { level: 3, placeholder: 'Title' }],
+						['core/paragraph', {
+							placeholder: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis.'
+						}],
+					]
+				],
+				['core/column', { "width": 40, className: 'internal-block-class' },
+					[
+						['core/heading',  { level: 3, placeholder: 'Title' } ],
+						['core/paragraph', {
+							placeholder: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent et eros eu felis.'
+						}],
+					]
+				],
+			],
+		}
+	);
